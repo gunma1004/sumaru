782ab57ad5b189950d92e243210f46d1daa47f1a
gunma1004 <hanabank94@naver.com>
2026-09-16 15:03:34 +0900

    소유확인

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   verification: {
     google: "", // 추후 구글 서치콘솔 인증 코드로 입력하세요
     other: {
-      "naver-site-verification": "e4e51d8f056a8bb9ca75bd713f374ab804521183",
+      "naver-site-verification": "4cffe0d9edbcb5a91310e1c705619f953618608b",
     },
   },
   openGraph: {
